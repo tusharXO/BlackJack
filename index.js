071abf7ccaa559efeea9da708bd612566575be83
tusharXO <iamtushar2004@gmail.com>
2023-04-17 19:01:37 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 let firstCard = 5
-let secondCard = 10
-
+let secondCard = 5
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let messageEl = document.getElementById("message-el")
+let message
+let sumEl = document.getElementById("sum-el")
+function startGame(){
+    sumEl.textContent +=" " + sum
 
-console.log(sum)
+    if(sum < 21){
+        message = "Do you want to draw another card"
+    }
+    else if(sum == 21){
+        message = "You got Blackjack!"
+    }
+    else{
+        message = "You are out of the game"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
